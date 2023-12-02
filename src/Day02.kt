2371d1bd7a273feb3d84616a8bbba86763518877
Niccolò Piazzesi <npiazzesi@gmail.com>
2023-12-02 15:42:27 +0100

    day 2

---
+++ src/Day02.kt
@@ -0,0 +1,62 @@
+import java.lang.Integer.max
+
+fun main() {
+
+
+    fun part1(input: List<String>): Int {
+        val redLimit = 12
+        val blueLimit = 14
+        val greenLimit = 13
+        return input.sumOf {
+            val parts = it.split(":")
+            val id = parts[0].split(" ")[1].toInt()
+            val subgames = parts[1].split(";")
+            val pred = subgames.all {
+                val g = it.split(",")
+                g.all {
+                    val p = it.trim().split(" ")
+                    val value = p[0].toInt()
+                    when (p[1]) {
+                        "green" -> value <= greenLimit
+                        "blue" -> value <= blueLimit
+                        "red" -> value <= redLimit
+                        else -> false
+                    }
+
+                }
+            }
+            if (pred) id else 0
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+
+        return input.sumOf {
+            val mins = mutableMapOf(
+                    "green" to 0,
+                    "blue" to 0,
+                    "red" to 0
+            )
+            val parts = it.split(":")
+            parts[0].split(" ")[1].toInt()
+            val subgames = parts[1].split(";")
+            subgames.forEach {
+                val g = it.split(",")
+                g.forEach {
+                    val p = it.trim().split(" ")
+                    val value = p[0].toInt()
+                    mins[p[1]] = max(mins[p[1]]!!, value)
+                }
+            }
+            mins["green"]?.times(mins["red"]!!)?.times(mins["blue"]!!)!!
+        }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    val input = readInput("Day02")
+    part1(testInput)
+    check(part2(testInput) == 2286)
+    part1(input).println()
+    part2(input).println()
+}
