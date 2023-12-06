a87d98aa0eae8cdccd595df74d1757990c618d53
Niccolò Piazzesi <npiazzesi@gmail.com>
2023-12-06 21:02:47 +0100

    day6 formatting

--- src/Day06.kt
+++ src/Day06.kt
@@ -5,26 +5,26 @@ fun main() {
         val distances = NUM.findAll(input[1]).map { it.value.toInt() }.toList()
         val timesToDist = times.zip(distances)
 
-        return timesToDist.fold(1) {mul, (time, distance) ->
+        return timesToDist.fold(1) { mul, (time, distance) ->
             var waysToWin = 0
-            for(i in 0..time){
-                if(i*(time-i) > distance){
+            for (i in 0..time) {
+                if (i * (time - i) > distance) {
                     waysToWin++
                 }
 
             }
-            mul*waysToWin
+            mul * waysToWin
         }
 
 
     }
 
     fun part2(input: List<String>): Long {
-        val time = NUM.findAll(input[0]).map { it.value}.joinToString("").toLong()
+        val time = NUM.findAll(input[0]).map { it.value }.joinToString("").toLong()
         val distance = NUM.findAll(input[1]).map { it.value }.joinToString("").toLong()
         var waysToWin = 0L
-        for(i in 0..time){
-            if(i*(time-i) > distance){
+        for (i in 0..time) {
+            if (i * (time - i) > distance) {
                 waysToWin++
             }
 
@@ -39,6 +39,6 @@ fun main() {
     check(part1(testInput) == 288)
     check(part2(testInput) == 71503L)
     val input = readInput("Day06")
-    println( "Part 1: ${part1(input)}")
-    println( "Part 2: ${part2(input)}")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
